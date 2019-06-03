354ead23be62698a67836619e913f6085378b4f7
Redzango <cjohns10@trinity.edu>
2019-06-03 14:22:55 -0500

    Sech2 works

--- 1DSolver.f90
+++ 1DSolver.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a6cc3f4a8eeccf32b65a50ef4613140d9739e74719befcc032f38f9d54f2126
-size 4890
+oid sha256:5ff7b8eefe01b39284bcb2597c31f088f7817aeb6ac14530630e822b835bb276
+size 4891
