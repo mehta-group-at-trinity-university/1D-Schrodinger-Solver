e8d51e6695c1bfe7aace4cc630e2b81e8439bdfb
Redzango <cjohns10@trinity.edu>
2019-06-06 12:13:02 -0500

    converted to sparse matrix, need eigen solver

--- 1DSolver.f90
+++ 1DSolver.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea0aecc340de28d616870b982b49a1dd1eef73f652644128232712d0476af5aa
-size 4891
+oid sha256:2758b1edb25960f2f9999fb6dc27ebc5b335de1b9ea5e835cf74c7448c3f1d74
+size 10302
