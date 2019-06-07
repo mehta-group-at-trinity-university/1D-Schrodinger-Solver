ec52d77c2422cf687807fd329dd5651aa9ff7caa
Redzango <cjohns10@trinity.edu>
2019-06-07 10:57:00 -0500

    i

--- 1DSolver.f90
+++ 1DSolver.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ac55a2a3aa386e51324f9cc73e07bec61355125f7db9edbef6d57cccc0c5504
-size 10288
+oid sha256:d18d7f0bbb3d0c6b450eb17e9c492c3d7358000ea7bb2b13f7f72267bdfda905
+size 10297
